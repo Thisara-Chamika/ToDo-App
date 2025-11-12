7e806ec8cb5112c3be1b38f3e998c8d1d9e61ee7
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-13 00:26:27 +0530

    create main functions

--- src/todoList/TodoList.jsx
+++ src/todoList/TodoList.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function TodoList() {
+
+    const [tasks, setTasks] = useState([]);
+    const [newTasks, setNewTask] = useState("");
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+    
+    function moveTaskDown(index){
+        
+    }
+
   return (
     <>
-    
+
     </>
   )
 }
