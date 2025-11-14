005064bee931591c73599e08bf57a7ef80fd0b92
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-14 08:47:41 +0530

    Added Functions

--- src/todoList/TodoList.jsx
+++ src/todoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 function TodoList() {
 
-    const [tasks, setTasks] = useState(["Create a linked post", "Complete task"]);
+    const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
@@ -11,18 +11,36 @@ function TodoList() {
 
     function addTask(){
 
+      if(newTask.trim() !== ""){
+        setTasks(t => [...t,newTask]);
+        setNewTask("")
+
+      }
     }
 
     function deleteTask(index){
 
+      const updatedTasks = tasks.filter((_,i) => i !== index)
+      setTasks(updatedTasks)
+
     }
 
     function moveTaskUp(index){
-
+        if(index > 0){
+          const updatedTasks = [...tasks];
+          [updatedTasks [index], updatedTasks[index-1]]=
+          [updatedTasks[index-1], updatedTasks[index]]
+          setTasks(updatedTasks);
+        }
     }
     
     function moveTaskDown(index){
-        
+        if(index < tasks.length - 1){
+          const updatedTasks = [...tasks];
+          [updatedTasks [index], updatedTasks[index + 1]]=
+          [updatedTasks[index + 1], updatedTasks[index]]
+          setTasks(updatedTasks);
+        }
     }
 
   return (
